12c207abe35c4ce2eb08c26546d086940cc0285b
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-24 18:59:49 +0000

    update auth string resources

--- lib/src/auth/resources/strings.dart
+++ lib/src/auth/resources/strings.dart
@@ -3,6 +3,9 @@ part of 'package:flutter_blog_app/src/auth/resources/auth_resources.dart';
 class _Strings {
   const _Strings._();
 
+  final String email = 'Email';
+  final String username = 'Username';
+  final String password = 'Password';
   final String signin = 'Sign in';
   final String signup = 'Sign up';
   final String haveAccount = 'Do you have an account?';
