4def1bd970c42be291fa92b01c6d6c07d9f93647
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-24 17:45:46 +0000

    add form scaffold layout widget

---
+++ lib/src/auth/ux/shared/form_scaffold.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog_app/core/utils/utils.dart';
+
+class FormScaffold extends StatelessWidget {
+  const FormScaffold({
+    required this.body,
+    this.appBar,
+    this.topBarHeight,
+    this.backgroundColor,
+    this.bottomNavBar,
+    this.padding,
+    super.key,
+  });
+
+  final Widget? appBar;
+  final double? topBarHeight;
+  final Widget body;
+  final Widget? bottomNavBar;
+  final Color? backgroundColor;
+  final EdgeInsetsGeometry? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    Utils.hideStatusBar();
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(topBarHeight ?? 55),
+        child: appBar ?? _sizedBox,
+      ),
+      body: GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        child: Form(
+          key: key,
+          child: Padding(
+            padding: padding ?? EdgeInsets.zero,
+            child: body,
+          ),
+        ),
+      ),
+      bottomNavigationBar:
+          bottomNavBar != null ? BottomAppBar(child: bottomNavBar) : null,
+    );
+  }
+
+  static const _width = 0.0;
+  static const _height = 0.0;
+
+  SizedBox get _sizedBox => const SizedBox(
+        width: _width,
+        height: _height,
+      );
+}
