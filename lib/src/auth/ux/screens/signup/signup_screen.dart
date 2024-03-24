5136dae074fa4e45e87138689caa9a3fc05b7110
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-24 19:00:39 +0000

    implement sign up screen

---
+++ lib/src/auth/ux/screens/signup/signup_screen.dart
@@ -0,0 +1,29 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blog_app/core/resources/dimens.dart';
+import 'package:flutter_blog_app/core/ux/widgets/custom_button.dart';
+import 'package:flutter_blog_app/core/ux/widgets/input_field.dart';
+import 'package:flutter_blog_app/src/auth/resources/auth_resources.dart';
+import 'package:flutter_blog_app/src/auth/ux/screens/signup/signup_screen_state.dart';
+import 'package:flutter_blog_app/src/auth/ux/shared/form_scaffold.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+
+part 'package:flutter_blog_app/src/auth/ux/screens/signup/widgets/signup_form.dart';
+
+class SignupScreen extends StatelessWidget {
+  SignupScreen({super.key});
+
+  final screenState = Get.lazyPut(SignupScreenState.new);
+
+  @override
+  Widget build(BuildContext context) {
+    return FormScaffold(
+      padding: const EdgeInsets.all(Dimens.nano),
+      key: key,
+      body: _form,
+    );
+  }
+
+  Widget get _form => SignupForm();
+}
