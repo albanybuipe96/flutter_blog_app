20888d94d851fafa12fa82033646b91c3b9febac
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-25 00:23:18 +0000

    refactored sign up screen

--- lib/src/auth/ux/screens/signup/widgets/signup_form.dart
+++ lib/src/auth/ux/screens/signup/widgets/signup_form.dart
@@ -1,29 +1,31 @@
 part of 'package:flutter_blog_app/src/auth/ux/screens/signup/signup_screen.dart';
 
-class SignupForm extends StatelessWidget {
+class SignupForm extends StatelessWidget with FormValidator, NavGraph {
   SignupForm({super.key});
 
   final state = Get.find<SignupScreenState>();
 
   @override
   Widget build(BuildContext context) {
-    // final state = Get.find<SignupScreenState>();
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          const Gap(Dimens.mega),
-          _signupTitle,
-          const Gap(Dimens.micro),
-          _usernameField,
-          const Gap(Dimens.micro),
-          _emailField,
-          const Gap(Dimens.micro),
-          _passwordField,
-          const Gap(Dimens.femto),
-          _signinPanel,
-          const Gap(Dimens.nano),
-          _signupButton,
-        ],
+      child: Form(
+        key: state.signupFormKey,
+        child: Column(
+          children: [
+            const Gap(Dimens.mega),
+            _signupTitle,
+            const Gap(Dimens.micro),
+            _usernameField,
+            const Gap(Dimens.micro),
+            _emailField,
+            const Gap(Dimens.micro),
+            _passwordField,
+            const Gap(Dimens.femto),
+            _signinPanel,
+            const Gap(Dimens.nano),
+            _signupButton,
+          ],
+        ),
       ),
     );
   }
@@ -38,6 +40,7 @@ class SignupForm extends StatelessWidget {
 
   Widget get _usernameField => InputField(
         controller: state.usernameController,
+        validator: nameValidator,
         fillColor: Colors.transparent,
         label: AuthResources.Strings.username,
         leading: const Icon(EvaIcons.personOutline),
@@ -47,6 +50,7 @@ class SignupForm extends StatelessWidget {
 
   Widget get _emailField => InputField(
         controller: state.emailController,
+        validator: emailValidator,
         fillColor: Colors.transparent,
         label: AuthResources.Strings.email,
         keyboardType: TextInputType.emailAddress,
@@ -58,6 +62,7 @@ class SignupForm extends StatelessWidget {
   Widget get _passwordField => Obx(
         () => InputField(
           controller: state.passwordController,
+          validator: passwordValidator,
           label: AuthResources.Strings.password,
           leading: const Icon(EvaIcons.lockOutline),
           obscure: state.isPassword(),
@@ -79,7 +84,7 @@ class SignupForm extends StatelessWidget {
           CustomButton.text(
             text: AuthResources.Strings.signin,
             style: const TextStyle(color: Colors.blue),
-            onPressed: () {},
+            onPressed: offToSignin,
           ),
         ],
       );
