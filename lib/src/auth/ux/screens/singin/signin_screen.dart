fef02703f36bc9c8784df309da6178045a7bc814
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-25 00:24:14 +0000

    implement sign in screen

---
+++ lib/src/auth/ux/screens/singin/signin_screen.dart
@@ -0,0 +1,31 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blog_app/core/resources/dimens.dart';
+import 'package:flutter_blog_app/core/resources/resources.dart';
+import 'package:flutter_blog_app/core/ux/widgets/custom_button.dart';
+import 'package:flutter_blog_app/core/ux/widgets/input_field.dart';
+import 'package:flutter_blog_app/src/auth/resources/auth_resources.dart';
+import 'package:flutter_blog_app/src/auth/ux/screens/singin/signin_screen_state.dart';
+import 'package:flutter_blog_app/src/auth/ux/shared/form_scaffold.dart';
+import 'package:flutter_blog_app/src/auth/ux/shared/form_validator.dart';
+import 'package:flutter_blog_app/src/shared/nav_graph.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+
+part 'package:flutter_blog_app/src/auth/ux/screens/singin/widgets/signin_form.dart';
+
+class SigninScreen extends StatelessWidget {
+  SigninScreen({super.key});
+
+  final screenState = Get.lazyPut(SigninScreenState.new);
+
+  @override
+  Widget build(BuildContext context) {
+    return FormScaffold(
+      padding: const EdgeInsets.all(Dimens.nano),
+      body: _form,
+    );
+  }
+
+  Widget get _form => SigninForm();
+}
