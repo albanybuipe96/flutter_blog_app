c1867cc5d458407688a29fb850c51cbdcf2ed10e
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-25 00:58:21 +0000

    add envied package implement secrets class

---
+++ lib/env/secrets.dart
@@ -0,0 +1,14 @@
+import 'package:envied/envied.dart';
+
+part 'secrets.g.dart';
+
+@Envied(obfuscate: true)
+class Secrets {
+  const Secrets._();
+
+  @EnviedField()
+  static String SUPABASE_URL = _Secrets.SUPABASE_URL;
+
+  @EnviedField()
+  static String SUPABASE_ANON_KEY = _Secrets.SUPABASE_ANON_KEY;
+}
