7c58ee66f098da62230eb6a2f0e9ffa89b7523c5
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-25 05:55:10 +0000

    update snack bars

--- lib/core/ux/widgets/snackbars.dart
+++ lib/core/ux/widgets/snackbars.dart
@@ -4,23 +4,29 @@ import 'package:get/get.dart';
 enum ErrorType { form, image, auth }
 
 class SnackBars {
-  static void error({ErrorType type = ErrorType.form, String? message}) {
+  static void error({
+    ErrorType type = ErrorType.form,
+    String? message,
+    void Function()? execute,
+  }) {
     switch (type) {
       case ErrorType.form:
         Get.snackbar(
           'Form validation error(s)',
           message ?? 'Some fields marked as required have not been filled.',
-          duration: const Duration(seconds: 2),
+          duration: const Duration(seconds: 5),
         );
       case ErrorType.image:
         Get.snackbar(
           'Form validation error(s)',
           'Pick a profile image to continue.',
-          duration: const Duration(seconds: 2),
+          duration: const Duration(seconds: 5),
         );
       case ErrorType.auth:
         Get.snackbar('Authentication Error', '$message');
     }
+
+    execute?.call();
   }
 
   static void success({String? message, void Function()? execute}) {
@@ -28,7 +34,7 @@ class SnackBars {
       'Success',
       message ?? 'Your account has been created successfully.',
       icon: const Icon(Icons.done_all_outlined),
-      duration: const Duration(seconds: 2),
+      duration: const Duration(seconds: 5),
     );
 
     execute?.call();
