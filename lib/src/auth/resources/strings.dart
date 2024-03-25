c218be412bd76e319a32e7c0fd5ddb39fdc9af59
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-25 00:22:28 +0000

    update auth strings resource

--- lib/src/auth/resources/strings.dart
+++ lib/src/auth/resources/strings.dart
@@ -10,4 +10,9 @@ class _Strings {
   final String signup = 'Sign up';
   final String haveAccount = 'Do you have an account?';
   final String havNoAccount = "You don't have an account?";
+  final String invalidUsername = "Username field can't be empty.";
+  final String invalidEmail = 'Invalid email address';
+  final String emptyEmailField = "Email field can't be empty";
+  final String invalidPassword = 'Password not strong enough';
+  final String emptyPasswordField = "Password field can't be empty.";
 }
