7b174e74a4e70b68e1923779b7dfd4ea251a4506
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-26 13:48:34 +0000

    refactored sign up screen

--- lib/src/auth/ux/screens/signup/signup_screen.dart
+++ lib/src/auth/ux/screens/signup/signup_screen.dart
@@ -1,26 +1,27 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_blog_app/core/resources/dimens.dart';
 import 'package:flutter_blog_app/core/ux/widgets/custom_button.dart';
 import 'package:flutter_blog_app/core/ux/widgets/input_field.dart';
-import 'package:flutter_blog_app/env/secrets.dart';
-import 'package:flutter_blog_app/src/auth/platform/data/repositories/auth_repository_impl.dart';
-import 'package:flutter_blog_app/src/auth/platform/data/sources/remote/auth_remote_data_source.dart';
-import 'package:flutter_blog_app/src/auth/platform/domain/usecases/signup_usecase.dart';
 import 'package:flutter_blog_app/src/auth/resources/auth_resources.dart';
+import 'package:flutter_blog_app/src/auth/ux/blocs/auth_bloc.dart';
 import 'package:flutter_blog_app/src/auth/ux/screens/signup/signup_screen_state.dart';
 import 'package:flutter_blog_app/src/auth/ux/shared/form_scaffold.dart';
 import 'package:flutter_blog_app/src/auth/ux/shared/form_validator.dart';
-import 'package:flutter_blog_app/src/shared/di.dart';
 import 'package:flutter_blog_app/src/shared/nav_graph.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+
 
 part 'package:flutter_blog_app/src/auth/ux/screens/signup/widgets/signup_form.dart';
 
 class SignupScreen extends StatelessWidget {
-  const SignupScreen({super.key});
+  SignupScreen({super.key});
+
+  final screenState = Get.lazyPut(
+    SignupScreenState.new,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -30,5 +31,5 @@ class SignupScreen extends StatelessWidget {
     );
   }
 
-  Widget get _form => SignupForm();
+  Widget get _form => const SignupForm();
 }
