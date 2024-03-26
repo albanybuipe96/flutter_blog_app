bc336fddf7c29e861e2c21b181d4cc6305cc43c0
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-26 13:49:15 +0000

    refactored main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_blog_app/core/resources/resources.dart';
 import 'package:flutter_blog_app/core/resources/theme.dart';
+import 'package:flutter_blog_app/src/auth/ux/blocs/auth_bloc.dart';
 import 'package:flutter_blog_app/src/auth/ux/screens/signup/signup_screen.dart';
 import 'package:flutter_blog_app/src/shared/di.dart';
 import 'package:get/get.dart';
@@ -10,6 +12,10 @@ import 'package:get/get.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Di.init();
+  runApp(
+    const MyApp(),
+  );
+
   runApp(const MyApp());
 }
 
@@ -22,7 +28,12 @@ class MyApp extends StatelessWidget {
       title: Strings.appName,
       debugShowCheckedModeBanner: false,
       theme: AppTheme.dark,
-      home: const SignupScreen(),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => serviceLocator<AuthBloc>()),
+        ],
+        child: SignupScreen(),
+      ),
     );
   }
 }
