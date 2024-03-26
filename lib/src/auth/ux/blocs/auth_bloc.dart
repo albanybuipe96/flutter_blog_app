10ddcafa6719e675b1375796129d47654c58382a
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-26 13:48:57 +0000

    update auth bloc to emit loading state

--- lib/src/auth/ux/blocs/auth_bloc.dart
+++ lib/src/auth/ux/blocs/auth_bloc.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_blog_app/src/auth/platform/domain/usecases/signup_usecase.dart';
@@ -16,7 +17,11 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
   final SignupUseCase _signupUsecase;
 
-  Future<void> _onSignupEvent(AuthSignup event, Emitter<AuthState> emit) async {
+  Future<void> _onSignupEvent(
+    AuthSignup event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
     final response = await _signupUsecase(
       SignupParams(
         username: event.username,
@@ -26,8 +31,14 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     );
 
     response.fold(
-      (failure) => emit(AuthFailure(message: failure.message)),
-      (uid) => emit(AuthSuccess(uid: uid)),
+      (failure) {
+        log(failure.message, name: '$runtimeType');
+        emit(AuthFailure(message: failure.message));
+      },
+      (uid) {
+        log(uid, name: '$runtimeType');
+        emit(AuthSuccess(uid: uid));
+      },
     );
   }
 }
