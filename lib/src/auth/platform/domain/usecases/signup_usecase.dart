e01904a747c0f7f87f7b1ac091198fb8d6dd0c8b
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-26 03:58:06 +0000

    add sign up use case

---
+++ lib/src/auth/platform/domain/usecases/signup_usecase.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_blog_app/core/errors/failures/failure.dart';
+import 'package:flutter_blog_app/core/platform/domain/usecases/parametarized_usecase.dart';
+import 'package:flutter_blog_app/src/auth/platform/domain/repositories/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class SignupUseCase implements ParametarizedUsecase<String, SignupParams> {
+  const SignupUseCase({required AuthRepository repository})
+      : _repository = repository;
+  final AuthRepository _repository;
+
+  @override
+  Future<Either<Failure, String>> call(SignupParams params) async {
+    return _repository.signup(
+      username: params.username,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class SignupParams {
+  const SignupParams({
+    required this.username,
+    required this.email,
+    required this.password,
+  });
+
+  factory SignupParams.empty() => const SignupParams(
+        username: '',
+        email: '',
+        password: '',
+      );
+
+  final String username;
+  final String email;
+  final String password;
+
+  SignupParams copyWith({
+    String? username,
+    String? email,
+    String? password,
+  }) {
+    return SignupParams(
+      username: username ?? this.username,
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'SignupParams{username: $username, '
+        'email: $email, password: $password}';
+  }
+}
