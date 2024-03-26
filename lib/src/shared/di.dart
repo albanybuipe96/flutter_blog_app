111b21afa8ac51837f1e185c439f3eb72a84b08e
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-26 05:55:18 +0000

    update di for bloc auth bloc dependencies

--- lib/src/shared/di.dart
+++ lib/src/shared/di.dart
@@ -6,8 +6,6 @@ import 'package:flutter_blog_app/src/auth/platform/data/sources/auth_data_source
 import 'package:flutter_blog_app/src/auth/platform/data/sources/remote/auth_remote_data_source.dart';
 import 'package:flutter_blog_app/src/auth/platform/domain/repositories/auth_repository.dart';
 import 'package:flutter_blog_app/src/auth/platform/domain/usecases/signup_usecase.dart';
-import 'package:flutter_blog_app/src/auth/ux/screens/signup/signup_screen_state.dart';
-import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -18,7 +16,6 @@ class Di {
 
   static Future<void> init() async {
     await _injector();
-    await _injectSignupState();
   }
 
   static Future<Supabase> initSupabase() async {
@@ -32,7 +29,7 @@ class Di {
     final client = (await Di.initSupabase()).client;
     serviceLocator
       ..registerFactory(
-        () => SignupScreenState(
+        () => (
           useCase: SignupUseCase(
             repository: serviceLocator(),
           ),
@@ -51,19 +48,5 @@ class Di {
       ..registerLazySingleton<SupabaseClient>(
         () => client,
       );
-  }
-
-  static Future<void> _injectSignupState() async {
-    Get.lazyPut(
-          () => SignupScreenState(
-        useCase: SignupUseCase(
-          repository: AuthRepositoryImpl(
-            dataSource: AuthRemoteDataSource(
-              client: serviceLocator(),
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
