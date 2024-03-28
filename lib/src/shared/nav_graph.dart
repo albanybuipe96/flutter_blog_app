7d5136f22f6faf9f0d9151867340897b9d1c9582
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-28 00:32:01 +0000

    updated nav graph

--- lib/src/shared/nav_graph.dart
+++ lib/src/shared/nav_graph.dart
@@ -1,21 +1,31 @@
 import 'package:flutter_blog_app/src/auth/ux/screens/signup/signup_screen.dart';
 import 'package:flutter_blog_app/src/auth/ux/screens/singin/signin_screen.dart';
+import 'package:flutter_blog_app/src/blog/ux/screens/home/home_screen.dart';
+import 'package:flutter_blog_app/src/blog/ux/screens/home/home_screen.dart';
 import 'package:get/get.dart';
 
 mixin class NavGraph {
-  void goToSignin() {
+  void goToSigninScreen() {
     Get.to<void>(SigninScreen.new);
   }
 
-  void offToSignin() {
+  void offToSigninScreen() {
     Get.off<void>(SigninScreen.new);
   }
 
-  void goToSignup() {
+  void goToSignupScreen() {
     Get.to<void>(SignupScreen.new);
   }
 
-  void offToSignup() {
+  void offToSignupScreen() {
     Get.off<void>(SignupScreen.new);
+  }
+
+  void goToHomeScreen() {
+    Get.to<void>(HomeScreen.new);
+  }
+
+  void offToHomeScreen() {
+    Get.off<void>(HomeScreen.new);
   }
 }
