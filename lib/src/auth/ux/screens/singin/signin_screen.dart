070524ea0fdcc1771b54a8926a036341f4e6b4ab
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-28 00:35:51 +0000

    updated sign in ui

--- lib/src/auth/ux/screens/singin/signin_screen.dart
+++ lib/src/auth/ux/screens/singin/signin_screen.dart
@@ -1,10 +1,13 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_blog_app/core/resources/dimens.dart';
 import 'package:flutter_blog_app/core/resources/resources.dart';
 import 'package:flutter_blog_app/core/ux/widgets/custom_button.dart';
 import 'package:flutter_blog_app/core/ux/widgets/input_field.dart';
+import 'package:flutter_blog_app/core/ux/widgets/snackbars.dart';
 import 'package:flutter_blog_app/src/auth/resources/auth_resources.dart';
+import 'package:flutter_blog_app/src/auth/ux/blocs/auth_bloc.dart';
 import 'package:flutter_blog_app/src/auth/ux/screens/singin/signin_screen_state.dart';
 import 'package:flutter_blog_app/src/auth/ux/shared/form_scaffold.dart';
 import 'package:flutter_blog_app/src/auth/ux/shared/form_validator.dart';
