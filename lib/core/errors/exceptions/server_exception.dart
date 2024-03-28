30eb2b13c8f4447942db972ca9bd4c72f4009f80
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-28 00:33:36 +0000

    modified server exception

--- lib/core/errors/exceptions/server_exception.dart
+++ lib/core/errors/exceptions/server_exception.dart
@@ -1,7 +1,9 @@
 class ServerException implements Exception {
   const ServerException({
     required this.message,
+    this.statusCode,
   });
 
   final String message;
+  final String? statusCode;
 }
