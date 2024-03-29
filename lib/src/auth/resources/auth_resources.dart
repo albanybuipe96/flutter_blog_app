947cde2f8a713a2a8e202b783667685b00da88cb
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-29 22:47:16 +0000

    updated to include styles

--- lib/src/auth/resources/auth_resources.dart
+++ lib/src/auth/resources/auth_resources.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/material.dart';
+
 part 'package:flutter_blog_app/src/auth/resources/images.dart';
 
 part 'package:flutter_blog_app/src/auth/resources/strings.dart';
+
+part 'package:flutter_blog_app/src/auth/resources/styles.dart';
 
 class AuthResources {
   const AuthResources._();
 
   static const Images = _Images._();
   static const Strings = _Strings._();
+  static final Styles = _Styles._();
 }
