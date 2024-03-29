62d5e304308c7c1e5e7751d15a36a9fd8ede64a6
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-29 13:21:09 +0000

    implemented sign out button and functionality

--- lib/src/blog/ux/screens/home/home_screen.dart
+++ lib/src/blog/ux/screens/home/home_screen.dart
@@ -1,6 +1,10 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_app/core/common/auth/current_user_state.dart';
+import 'package:flutter_blog_app/src/shared/nav_graph.dart';
+import 'package:get/get.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatelessWidget with NavGraph {
   const HomeScreen({super.key});
 
   @override
@@ -8,9 +12,22 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: _title,
+        actions: _actions,
       ),
     );
   }
 
   Widget get _title => const Text('Home Screen');
+
+  List<Widget> get _actions {
+    final currentUserState = Get.find<CurrentUserState>();
+    return [
+      IconButton(
+        onPressed: () => currentUserState.signout(
+          navigateTo: offToSigninScreen,
+        ),
+        icon: const Icon(EvaIcons.logOutOutline),
+      ),
+    ];
+  }
 }
