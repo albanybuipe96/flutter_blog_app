f7f0395e35b05e8f7574a059bde800474a2e7940
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-29 22:42:32 +0000

    added styles resources to auth resources

---
+++ lib/src/auth/resources/styles.dart
@@ -0,0 +1,10 @@
+part of 'package:flutter_blog_app/src/auth/resources/auth_resources.dart';
+
+class _Styles {
+  _Styles._();
+
+  final authPanelButtonStyle = TextStyle(
+    color: Colors.blue.shade500,
+    fontWeight: FontWeight.bold,
+  );
+}
