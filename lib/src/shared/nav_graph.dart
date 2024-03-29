fe895fe079f0c5faff045e8575162833570c3d7b
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-29 14:11:25 +0000

    update: added to new blog screen navigation

--- lib/src/shared/nav_graph.dart
+++ lib/src/shared/nav_graph.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_blog_app/src/auth/ux/screens/signin/signin_screen.dart';
 import 'package:flutter_blog_app/src/auth/ux/screens/signup/signup_screen.dart';
+import 'package:flutter_blog_app/src/blog/ux/screens/add/add_blog_screen.dart';
 import 'package:flutter_blog_app/src/blog/ux/screens/home/home_screen.dart';
 import 'package:get/get.dart';
 
@@ -26,5 +27,13 @@ mixin class NavGraph {
 
   void offToHomeScreen() {
     Get.off<void>(HomeScreen.new);
+  }
+
+  void goToAddBlogScreen() {
+    Get.to<void>(AddBlogScreen.new);
+  }
+
+  void offToAddBlogScreen() {
+    Get.off<void>(AddBlogScreen.new);
   }
 }
