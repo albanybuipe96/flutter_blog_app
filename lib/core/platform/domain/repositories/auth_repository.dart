16540f05c4753abfb1229a3360e508ac1e346154
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-29 13:16:03 +0000

    refactor: moved auth repo interface to core

--- lib/core/platform/domain/repositories/auth_repository.dart
+++ lib/core/platform/domain/repositories/auth_repository.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_blog_app/core/errors/failures/failure.dart';
-import 'package:flutter_blog_app/src/auth/platform/domain/entities/user.dart';
-import 'package:flutter_blog_app/src/auth/platform/domain/entities/user.dart';
+import 'package:flutter_blog_app/core/platform/entities/user.dart';
 import 'package:fpdart/fpdart.dart';
 
 abstract interface class AuthRepository {
+  Future<Either<Failure, User>> get currentUser;
+
   Future<Either<Failure, User>> signin({
     required String email,
     required String password,
@@ -14,4 +15,6 @@ abstract interface class AuthRepository {
     required String email,
     required String password,
   });
+
+  Future<Either<Failure, void>> signout();
 }
