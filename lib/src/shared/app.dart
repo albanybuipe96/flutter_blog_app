9f06558e8d4f5e38f43323f559b6dfbc1ea922c0
albanybuipe96 <mohammedarahman96@gmail.com>
2024-03-29 13:20:32 +0000

    added app state

---
+++ lib/src/shared/app.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog_app/core/common/auth/current_user_state.dart';
+import 'package:flutter_blog_app/core/resources/resources.dart';
+import 'package:flutter_blog_app/src/auth/ux/screens/signin/signin_screen.dart';
+import 'package:flutter_blog_app/src/auth/ux/screens/signup/signup_screen.dart';
+import 'package:flutter_blog_app/src/blog/ux/screens/home/home_screen.dart';
+import 'package:flutter_blog_app/src/shared/di.dart';
+import 'package:get/get.dart';
+
+class App extends StatelessWidget {
+  App({super.key});
+
+  final currentUserState = Get.put(AppState());
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: Strings.appName,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.dark,
+      home: _home,
+    );
+  }
+
+  Widget get _home {
+    final appState = Get.find<AppState>();
+    return Obx(
+      () {
+        return appState.isUserLoggedIn() ? const HomeScreen() : SigninScreen();
+      },
+    );
+  }
+}
+
+class AppState extends GetxController {
+  final currentUserState = Get.put(
+    CurrentUserState(
+        currentUserUsecase: serviceLocator(), signoutUsecase: serviceLocator()),
+  );
+
+  bool isUserLoggedIn() => currentUserState.isUserLoggedIn();
+}
